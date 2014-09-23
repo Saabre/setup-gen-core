c4bf64c10398e495e740572e6190989f0b1b9e0b
ElouanSaabre <elouan@saabre.com>
2014-09-23 10:31:58 +0200

    Initial commit

---
+++ src/com/saabre/setup/SetupGen.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.saabre.setup;
+
+/**
+ *
+ * @author Lifaen
+ */
+public class SetupGen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
