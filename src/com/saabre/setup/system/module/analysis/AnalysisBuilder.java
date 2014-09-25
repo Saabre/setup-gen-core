3209c1553e427f58f5297c5e4526899b7b928ba8
ElouanSaabre <elouan@saabre.com>
2014-09-25 18:46:16 +0200

    Add R Analysis module

---
+++ src/com/saabre/setup/system/module/analysis/AnalysisBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.saabre.setup.system.module.analysis;
+
+import org.rosuda.JRI.Rengine;
+
+/**
+ *
+ * @author Lifaen
+ */
+public class AnalysisBuilder {
+    
+    // -- Attributes --
+    
+    private Rengine re;
+    
+    // -- Methods --
+    
+    public void load() throws Exception
+    {
+        re = new Rengine (new String [] {"--vanilla"}, false, null);
+        if (!re.waitForR())
+            throw new Exception("Cannot load R");
+    }
+    
+    public void cd(String path)
+    {
+        re.eval("setwd(\""+ path +"\")");
+    }
+    
+    public void render(String file)
+    {
+        re.eval("rmarkdown::render(\""+ file +".Rmd\")");
+    }
+    
+    public void close()
+    {
+        re.end();
+    }
+    
+}
---
+++ src/com/saabre/setup/system/module/analysis/AnalysisModule.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.saabre.setup.system.module.analysis;
+
+import com.saabre.setup.system.base.Module;
+import org.rosuda.JRI.Rengine;
+
+/**
+ *
+ * @author Lifaen
+ */
+public class AnalysisModule extends Module {
+
+    @Override
+    protected void preRun() throws Exception 
+    {
+        
+    }
+
+    @Override
+    protected void run() throws Exception 
+    {
+        
+    }
+
+    @Override
+    protected void postRun() throws Exception 
+    {
+        
+    }
+    
+}
