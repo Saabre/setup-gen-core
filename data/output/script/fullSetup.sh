3c7f5d716aa75fdc955d7ec3270d0683ffcb3912
ElouanSaabre <elouan@saabre.com>
2014-09-26 09:38:51 +0200

    Change private configuration

--- data/output/script/fullSetup.sh
+++ data/output/script/fullSetup.sh
@@ -3,14 +3,14 @@
 # -- Operation AllocateSwap --
 
     # Allocation de la mémoire --
-    sudo dd if=/dev/zero of=/mnt/swap.2014-09-25.16-50-18.2Gio bs=1024 count=2097152
-    sudo chmod 600 /mnt/swap.2014-09-25.16-50-18.2Gio
+    sudo dd if=/dev/zero of=/mnt/swap.2014-09-25.18-50-20.2Gio bs=1024 count=2097152
+    sudo chmod 600 /mnt/swap.2014-09-25.18-50-20.2Gio
 
     # Formatage --
-    sudo mkswap /mnt/swap.2014-09-25.16-50-18.2Gio
+    sudo mkswap /mnt/swap.2014-09-25.18-50-20.2Gio
 
     # Utilisation --
-    sudo swapon /mnt/swap.2014-09-25.16-50-18.2Gio
+    sudo swapon /mnt/swap.2014-09-25.18-50-20.2Gio
 
     # Verification --
     cat /proc/meminfo
